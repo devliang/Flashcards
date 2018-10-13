bee4298b7434139c1fe698928a7ea274c4aa2acf
devliang <ryanfang0827@gmail.com>
2018-10-13 19:58:12 -0400

    Simple Flashcards Lab Done

---
+++ Flashcards/Flashcards/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Ryan Fang on 10/13/18.
+//  Copyright © 2018 Liang Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true;
+    }
+    
+}
+
